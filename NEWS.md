ad63ca0ce1a64b0770c7066227cd02fc6a576c6f
Basil Okola <33178339+Bokola@users.noreply.github.com>
2024-12-24 15:54:13 +0300

    update

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# rapR (development version)
+
+* Initial CRAN submission.
---
+++ R/globals.R
@@ -0,0 +1,13 @@
+#' @importFrom utils globalVariables
+#' 
+utils::globalVariables(
+  c(
+    'locality',
+    'year',
+    'average_price_nominal_euros',
+    'p0',
+    'average_price_m2_nominal_euros',
+    'p0_m2',
+    'pl_m2'
+  )
+)
